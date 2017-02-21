190ab0390c55aaaff47a46d3817752896ec5ea42
athiel <athiel@PC1010>
2017-02-21 09:11:38 +0100

    Add project files.

---
+++ XamBanque/XamBanque/Views/LoginPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamBanque.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        protected virtual void OnResume()
+        {
+
+
+        }
+
+        private async void ConnexionButton_OnClickedAsync(object sender, EventArgs e)
+        {
+            connexionButton.IsEnabled = false;
+
+            var login = loginEditText.Text;
+            var passWord = passwordEditText.Text;
+
+       
+
+            if ("user".Equals(login))
+            {
+                if ("user".Equals(passWord))
+                {
+                   // await Navigation.PushModalAsync(new MainPage());
+                    connexionButton.IsEnabled = true;
+
+                }
+                else
+                {
+                    await DisplayAlert(null, "Incorrect password", "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert(null, "Incorrect Login", "OK");
+            }
+        }
+
+    }
+}
